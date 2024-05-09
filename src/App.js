9e6da8da90ff1106a1ea187c40715d92ffea99f1
Julian <juha@acer.speedport.ip>
2024-05-09 10:38:37 +0200

    added new header and logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-
+import * as React from "react";
 import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import {
   ErrorBoundary,
@@ -16,7 +16,6 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
 import {
   buildAutocompleteQueryConfig,
   buildFacetConfigFromConfig,
@@ -25,6 +24,26 @@ import {
   getConfig,
   getFacetFields
 } from "./config/config-helper";
+
+//MUI
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Badge from '@mui/material/Badge';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import AdbIcon from '@mui/icons-material/Adb';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+import architects_logo from './assets/architects_logo.png'
 
 const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 
@@ -44,51 +63,198 @@ const config = {
   alwaysSearchOnInitialLoad: true
 };
 
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#113537',
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#E0C2FF',
+      light: '#F5EBFF',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#FAFAFF',
+    },
+  },
+});
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+
 export default function App() {
 
   return (
-    <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={<SearchBox autocompleteSuggestions={true} />}
-                  sideContent={
-                    <div>
-                      {wasSearched && (
-                        <Sorting
-                          label={"Sort by"}
-                          sortOptions={buildSortOptionsFromConfig()}
-                        />
-                      )}
-                      {getFacetFields().map(field => (
-                        <Facet key={field} field={field} label={field} />
-                      ))}
-                    </div>
-                  }
-                  bodyContent={
-                    <Results
-                      titleField={getConfig().titleField}
-                      urlField={getConfig().urlField}
-                      thumbnailField={getConfig().thumbnailField}
-                      shouldTrackClickThrough={true}
-                    />
-                  }
-                  bodyHeader={
-                    <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
-                    </React.Fragment>
-                  }
-                  bodyFooter={<Paging />}
+    <div>
+      {/* MUI Layout  */}
+      <ThemeProvider theme={theme}>
+        {/* change margin hier später in der box */}
+
+
+        <Box sx={{ flexGrow: 1 }} >
+          {/* baseline entfernt margin */}
+          <CssBaseline />
+          <AppBar position="static"  >
+
+
+
+
+            <Toolbar>
+
+              <Box
+                component="img"
+                alt="Logo"
+                sx={{ border: 0, height: 54, width: 54 }}
+                src={architects_logo}
+              />
+
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
                 />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider>
+              </Search>
+
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                //   onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </ThemeProvider>
+
+      {/* starte 2 container here */}
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+
+            <Box sx={{ bgcolor: '#D9D9D9', height: '100vh' }} />
+
+          </Grid>
+
+        <Grid item xs={6}>
+
+            <Box sx={{ bgcolor: '#AD6A6C', height: '100vh' }} />
+            
+
+          
+      
+
+
+      {/* ELS Layout */}
+
+      <SearchProvider config={config}>
+        <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+          {({ wasSearched }) => {
+            return (
+              <div className="App">
+                <ErrorBoundary>
+                  <Layout
+                    header={<SearchBox autocompleteSuggestions={true} />}
+                    sideContent={
+                      <div>
+                        {wasSearched && (
+                          <Sorting
+                            label={"Sort by"}
+                            sortOptions={buildSortOptionsFromConfig()}
+                          />
+                        )}
+                        {getFacetFields().map(field => (
+                          <Facet key={field} field={field} label={field} />
+                        ))}
+                      </div>
+                    }
+
+                    bodyContent={
+                      <Results
+                        titleField={getConfig().titleField}
+                        urlField={getConfig().urlField}
+                        thumbnailField={getConfig().thumbnailField}
+                        shouldTrackClickThrough={true}
+                      />
+                    }
+                    bodyHeader={
+                      <React.Fragment>
+                        {wasSearched && <PagingInfo />}
+                        {wasSearched && <ResultsPerPage />}
+                      </React.Fragment>
+                    }
+                    bodyFooter={<Paging />}
+                  />
+                </ErrorBoundary>
+              </div>
+            );
+          }}
+        </WithSearch>
+      </SearchProvider>
+
+      </Grid>
+
+
+      </Grid>
+
+
+    </div>
   );
 }
