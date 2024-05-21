81164459df9065a65ef84841bedbc8f9cd1acc8f
Julian <juha@acer.speedport.ip>
2024-05-21 15:25:42 +0200

    added 2 configs, dev and prod

--- src/App.js
+++ src/App.js
@@ -9,20 +9,15 @@ import {
   SearchBox,
   Results,
   PagingInfo,
-  ResultsPerPage,
   Paging,
   Sorting,
   WithSearch
 } from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import {
   buildAutocompleteQueryConfig,
-  buildFacetConfigFromConfig,
   buildSearchOptionsFromConfig,
-  buildSortOptionsFromConfig,
   getConfig,
-  getFacetFields
 } from "./config/config-helper";
 
 //MUI
@@ -30,22 +25,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
 import { styled, alpha } from '@mui/material/styles';
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import AdbIcon from '@mui/icons-material/Adb';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 import architects_logo from './assets/architects_logo.png'
 
-const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
+//  required for ELS cloud config
+//  const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 
 const connector = new ElasticsearchAPIConnector({
   host: "http://localhost:9200",
@@ -54,7 +44,10 @@ const connector = new ElasticsearchAPIConnector({
 
 const config = {
   searchQuery: {
-    facets: buildFacetConfigFromConfig(),
+    disjunctiveFacets: ["reporter.keyword"],
+    facets: {
+      "reporter.keyword": { type: "value" }
+    },
     ...buildSearchOptionsFromConfig()
   },
 
@@ -97,64 +90,51 @@ const Search = styled('div')(({ theme }) => ({
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
+function Item(props) {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
 
 export default function App() {
 
   return (
     <SearchProvider config={config}>
 
-        {/* MUI Layout  */}
-        <ThemeProvider theme={theme}>
-          {/* change margin hier später in der box */}
+      {/* MUI Layout  */}
+      <ThemeProvider theme={theme}>
 
-          <Box sx={{ flexGrow: 1 }} >
-            {/* baseline entfernt margin */}
-            <CssBaseline />
-            <AppBar position="static"  >
+        {/* Parent Box for header */}
+        <Box sx={{
+          display: 'flex', flexDirection: 'column'
+        }}>
 
-              <Toolbar>
+          {/* CssBaseline entfernt margin */}
+          <CssBaseline />
+          <AppBar position="static"  >
 
-                <Box
-                  component="img"
-                  alt="Logo"
-                  sx={{ border: 0, height: 54, width: 54 }}
-                  src={architects_logo}
-                />
+            <Toolbar>
 
+              <Box
+                component="img"
+                alt="Logo"
+                sx={{ display: 'flex', border: 0, height: 54, width: 54 }}
+                src={architects_logo}
+              />
+
+              <Box sx={{ flexGrow: 1, padding: '0rem 15rem' }}>
                 <Search>
-                  <SearchIconWrapper>
-                    <SearchIcon />
-                  </SearchIconWrapper>
-                  <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                  />
+                  <SearchBox autocompleteSuggestions={true} />
                 </Search>
+              </Box>
 
+              <Item>
                 <IconButton
                   size="large"
                   aria-label="show 17 new notifications"
@@ -164,7 +144,9 @@ export default function App() {
                     <NotificationsIcon />
                   </Badge>
                 </IconButton>
+              </Item>
 
+              <Item>
                 <IconButton
                   size="large"
                   aria-label="account of current user"
@@ -176,81 +158,89 @@ export default function App() {
                   <AccountCircle />
                 </IconButton>
 
-              </Toolbar>
-            </AppBar>
+              </Item>
+
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </ThemeProvider>
+
+      {/* 2 containers filter and result start here*/}
+      <Grid container spacing={2}>
+
+        {/* Filter Box */}
+        <Grid item xs={4}>
+
+          <Box sx={{ bgcolor: '#D9D9D9', height: '90vh' , padding: '2rem 2rem 0rem 2rem' }}>
+
+            <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+              {({ wasSearched }) => {
+                return (
+
+                  <ErrorBoundary>
+                    <Sorting
+                      label={"Sort by"}
+                      sortOptions={[
+                        {
+                          name: "Reporter: A -> Z",
+                          value: [{ field: "reporter.keyword", direction: "asc" }]
+                        },
+                        {
+                          name: "Reporter: Z -> A",
+                          value: [{ field: "reporter.keyword", direction: "desc" }]
+                        }
+
+                      ]}
+                    />
+                    <Box sx={{ border: 'border: 15px solid green' }}>
+
+                      <Facet key={"1"} field={"reporter.keyword"} label={"Reporter"} />
+
+                    </Box>
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo />}
+                      {/* {wasSearched && <ResultsPerPage />} */}
+                    </React.Fragment>
+                  </ErrorBoundary>
+
+                )
+              }}
+            </WithSearch>
+
+
           </Box>
-        </ThemeProvider>
-
-        {/* 2 containers search and result start here*/}
-        <Grid container spacing={2}>
-
-          {/* Search Box */}
-          <Grid item xs={6}>
-
-            <Box sx={{ bgcolor: '#D9D9D9', height: '100vh' }}>
-              <SearchBox autocompleteSuggestions={true} />
-            </Box>
-
-          </Grid>
-
-          {/* Result Box */}
-          <Grid item xs={6}>
-
-            <Box sx={{ bgcolor: '#AD6A6C', height: '100vh' }}>
-
-
-
-              <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-                {({ wasSearched }) => {
-                  return (
-                    <div className="App">
-
-                      <ErrorBoundary>
-                        <div>
-                          {wasSearched && (
-                            <Sorting
-                              label={"Sort by"}
-                              sortOptions={buildSortOptionsFromConfig()}
-                            />
-                          )}
-                          {getFacetFields().map(field => (
-                            <Facet key={field} field={field} label={field} />
-                          ))}
-                        </div>
-
-
-
-                        <Results
-                          titleField={getConfig().titleField}
-                          urlField={getConfig().urlField}
-                          thumbnailField={getConfig().thumbnailField}
-                          shouldTrackClickThrough={true}
-                        />
-
-
-                        <React.Fragment>
-                          {wasSearched && <PagingInfo />}
-                          {wasSearched && <ResultsPerPage />}
-                        </React.Fragment>
-                      </ErrorBoundary>
-
-                    </div>
-
-                  )
-                }}
-              </WithSearch>
-
-
-              <Paging />
-
-            </Box>
-
-
-
-          </Grid>
 
         </Grid>
 
+        {/* Result Box */}
+        <Grid item xs={8}>
+          <Box sx={{ bgcolor: '#AD6A6C', height: '90vh' }}>
+
+            <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+              {({ wasSearched }) => {
+                return (
+
+                  <ErrorBoundary>
+
+                    <Box sx={{ padding: '1rem 2rem 0rem 2rem' }}>
+                      <Results
+
+                        titleField={getConfig().titleField}
+                        urlField={getConfig().urlField}
+                        thumbnailField={getConfig().thumbnailField}
+                        shouldTrackClickThrough={true}
+
+                      />
+                    </Box>
+                  </ErrorBoundary>
+
+                )
+              }}
+            </WithSearch>
+            <Paging />
+          </Box>
+        </Grid>
+      </Grid>
     </SearchProvider>
   );
 }
